f7c4cda9594c96b14f721f20b780299017fb7705
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-07-11 12:18:21 +0000

    Update Bazel to latest version

--- datasets/isic16.py
+++ datasets/isic16.py
@@ -191,4 +191,22 @@ def load_dataset(args, fold, train=True, aug_k=40, aug_n=1, patch=False):
         [
             transforms.Resize(size),
             transforms.ToTensor(),
-            normali
+            normalize,
+        ]
+    )
+    test_label_transform = transforms.Compose(
+        [
+            transforms.Resize(size, interpolation=Image.NEAREST),
+            transforms.ToTensor(),
+        ]
+    )
+
+    train_dataset = ISIC16(
+        X_train,
+        y_train,
+        names_train,
+        im_transform=transform,
+        label_transform=label_transform,
+        train=True,
+    )
+    val_d
