1dac8bca58319d0cc205cb0656842035c3f8cb0f
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-03-17 11:20:19 +0000

    Run scan-local.sh on chronograf:...

--- run.py
+++ run.py
@@ -10,4 +10,17 @@ from lightning.fabric.utilities.seed import seed_everything
 
 import wandb
 from utils.train_utils import get_rank
-from workers.test_net i
+from workers.test_net import test_aug_worker, test_worker
+from workers.train_net import train_aug_worker, train_worker
+
+torch.set_float32_matmul_precision("high")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="2D Medical Image Segmentation")
+
+    parser.add_argument(
+        "--cfg",
+        default="./configs.yaml",
+        type=str,
+        help="Config file used for experiment",
