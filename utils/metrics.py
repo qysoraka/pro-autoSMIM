a50a20cdb46b7ec1b58ffde481abd0869f873386
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-05-23 20:41:54 +0000

    Fix quality flaw

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,21 @@
 from multiprocessing.pool import Pool
 
-from typing import Any, Ca
+from typing import Any, Callable, Iterable, List, Set, Tuple, TypeVar, Union
+
+import torch
+import numpy as np
+from torch import einsum
+from torch import Tensor
+from functools import partial
+from scipy.ndimage import distance_transform_edt as distance
+from scipy.spatial.distance import directed_hausdorff
+
+EPS = 1e-7
+
+
+# Assert utils
+def uniq(a: Tensor) -> Set:
+    return set(torch.unique(a.cpu()).numpy())
+
+
+def sset(a: Ten
