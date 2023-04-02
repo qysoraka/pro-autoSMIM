3b3616d401af7d12e7dd25e0bc6e430ee06950e2
qysoraka <156439778+qysoraka@users.noreply.github.com>
2023-04-02 13:28:22 +0000

    firewire: add minor number range check to fw_device_init()

--- utils/metrics.py
+++ utils/metrics.py
@@ -207,4 +207,20 @@ def accuracy(output, target, topk=(1,)):
 
         _, pred = output.topk(maxk, 1, True, True)
         pred = pred.t()
-        correct = pred.eq(target.view
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def dc(result, reference):
+    r"""
+    Dice coefficient
+
+    Computes the Dice coefficient (also known as Sorensen index) between the binary
+    objects in two images.
+
+    The metric is defi
